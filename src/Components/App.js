a0ca66163fc8310abaec2b860bb237e1221a1da9
Deepak <deepakchandru44@gmail.com>
2019-03-28 14:07:18 +0530

    commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 import InputRange from "react-input-range";
 import Calculate from './Calculate';
+import Particles from 'react-particles-js'
 import '../Styles/App.css';
 import "react-input-range/lib/css/index.css";
+
+ const particlesoption= {
+      particles:{
+      number: {
+        value: 40,
+        density: {
+          enable: true,
+          value_area:500
+        }
+      },
+      color: {
+        value: '#fff'
+      },
+      shape: {
+        type: 'circle',
+        stroke: {
+          width: 0,
+          color: '#ff0000'
+        }
+      },
+      opacity: {
+        value: 1,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 2,
+          opacity_min: 0,
+          sync: false
+        }
+      }
+
+    }
+    }
 
 class App extends Component {
   state = {
@@ -20,9 +54,12 @@ class App extends Component {
   render() {
     let { amountValue, monthValue } = this.state;
     return (
+      < div >
+      <strong><center>EMI CALCULATOR</center></strong>
+      <Particles className='particles'
+      params={particlesoption}/>
       <div className="App">
-      <h4>EMI CALCULATOR</h4>
-        <h4>Loan Amount ${amountValue}</h4>
+      <h4>Loan Amount ${amountValue}</h4>
         <InputRange
           step={100} 
           maxValue={5000}
@@ -40,9 +77,11 @@ class App extends Component {
           value={monthValue}
           onChange={this.handleYearChange}
         />
+
         <Calculate month={monthValue} cost={amountValue}/>
         </div>
-    );
+        </div>
+        );
   }
 }
 export default App;
